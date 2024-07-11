b6dde34f15e8e14b9732ef12ebe160a189b14c0d
Camila T <camiladamonte@gmail.com>
2024-07-11 07:58:26 -0300

    borrar imports sin uso

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import NavBar from './Components/NavBar'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
